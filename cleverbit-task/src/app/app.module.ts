afd81fbd0959da9ad4ba1a4db11c7467dbb32801
Mel <melbryanflores316@hotmail.com>
2021-05-17 21:47:35 +0800

    bootstrapped angular

--- cleverbit-task/src/app/app.module.ts
+++ cleverbit-task/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ScoreBoardComponent } from './components/score-board/score-board.component';
+import { ArenaComponent } from './components/arena/arena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ScoreBoardComponent,
+    ArenaComponent
   ],
   imports: [
     BrowserModule,
